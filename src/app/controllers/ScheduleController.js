0335f4370f88071fa11f6c5248d1fd025f2c0b9b
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2020-02-05 10:37:12 -0300

    Listando agenda do Prestador

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,34 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
+import AppoiUserntment from '../models/User';
+import Appointment from '../models/Appointment';
+
+class ScheduleController {
+  async index(req, res) {
+    const checkUserProvider = await AppoiUserntment.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkUserProvider) {
+      return res.status(401).json({ error: 'User is not a provider' });
+    }
+
+    const { date } = req.query;
+    const parsedDate = parseISO(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
+        },
+      },
+      order: ['date'],
+    });
+
+    return res.json(appointments);
+  }
+}
+
+export default new ScheduleController();
